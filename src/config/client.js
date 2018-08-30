8322d70e3cf06cd77a796fdc4733b0a11de122dd
Ryan Megidov <ryan@starcoders.com>
2018-08-31 01:14:07 +0200

    FIX - initial render

--- src/config/client.js
+++ src/config/client.js
@@ -1,10 +1,9 @@
 // This is the entry point for our client-side logic
 import 'core/polyfills'
-
-//@TODO: Add some styling
-//import '../assets/css/index.css'
+import '../assets/css/index.css'
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from './redux'
@@ -12,7 +11,7 @@ import Index from '../pages/layout/Index'
 
 // Render our component according to our routes
 function renderApp() {
-  React.render((
+  ReactDOM.render((
     <Provider store={store}>
       <BrowserRouter>
         <Index/>
