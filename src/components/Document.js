fb73662ad0839ceccd49edf56ae7469ef6192c27
Ryan Megidov <ryan@starcoders.com>
2018-08-31 02:30:46 +0200

    ADD - placeholder components

---
+++ src/components/Document.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+@connect()
+class Document extends Component {
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Document</h1>
+      </div>
+    )
+  }
+}
+
+export default Document
---
+++ src/components/DocumentList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+@connect()
+class DocumentList extends Component {
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>DocumentList</h1>
+      </div>
+    )
+  }
+}
+
+export default DocumentList
